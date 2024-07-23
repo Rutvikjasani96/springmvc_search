f3de38d573ea8af3d7d222f70b858c858fe61a8a
Rutvik Jasani <rutvikjasani96@gmail.com>
2024-07-23 14:10:33 +0530

    Search Web Application using redirectView is Completed.

---
+++ src/main/java/com/springmvc_search/controller/SearchController.java
@@ -0,0 +1,28 @@
+package com.springmvc_search.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class SearchController {
+    @RequestMapping("/home")
+    public String home(){
+        System.out.println("Home Calling...");
+        return "home";
+    }
+
+    @RequestMapping("/searched")
+    public RedirectView searched(@RequestParam("querybox") String query){
+        System.out.println("Searched Calling...");
+        if(query.isBlank()){
+            return new RedirectView("home");
+        }
+        RedirectView redirectView = new RedirectView();
+        String url = "https://www.google.com/search?q="+query;
+        redirectView.setUrl(url);
+        return redirectView;
+    }
+
+}
