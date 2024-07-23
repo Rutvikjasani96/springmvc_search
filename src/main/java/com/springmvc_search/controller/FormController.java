4aa33a851a0e4dd11cc1c2123d7b257b6f015cfe
Rutvik Jasani <rutvikjasani96@gmail.com>
2024-07-23 17:55:05 +0530

    Handling Complex Form in Spring MVC

---
+++ src/main/java/com/springmvc_search/controller/FormController.java
@@ -0,0 +1,22 @@
+package com.springmvc_search.controller;
+
+import com.springmvc_search.entities.Student;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class FormController {
+
+    @RequestMapping("/complex")
+    public String showForm(){
+        return "complex_form";
+    }
+
+    @RequestMapping(path = "/handleform",method = RequestMethod.POST)
+    public String formHandler(@ModelAttribute Student student){
+        System.out.println(student);
+        return "success";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/success.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Rutvik Jasani
+  Date: 23/07/2024
+  Time: 17:29
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@page isELIgnored="false" %>
+<html>
+<head>
+    <title>Success Registration</title>
+</head>
+<body>
+    <h1>${student}</h1>
+</body>
+</html>
